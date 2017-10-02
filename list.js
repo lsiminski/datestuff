6b57a8acb5a8014f6b0f3ea0bdfd9c6f37b190bf
lsiminski <lsiminski@mica.edu>
2017-10-02 17:19:02 -0400

    asdcasdc

--- list.js
+++ list.js
@@ -1,15 +1,18 @@
 $.getJSON('list.json', function(data) { 
 
- var d = new Date();
- var n = d.getHour();
- var moda = n;
+  var d = new Date();
+  var mo = d.getMonth();
+  var da = d.getDate ();
+  var ho = d.getHours ();
+  var moda = ho + 1;
     
   $.each(data, function(data) {
-	  if ( this.hour === n ) {
+	  if ( this.hour === moda ) {
 		  $("#list_name p").html("<a href='" + this.url + "' target=blank>" + this.name + "</a>");
 		  $("#list_day p").text(this.day);
-		  $("#list_hour p").text(this.hour);
 		  $("#list_category p").text(this.category);
+		  $("#list_hour p").text(this.hour);
+// 		  $("body.random").css("background-image","url(" + this.image + ")");
 	  } else {
 	  }
   });
