a000836b517c3796d558a6aa3d13efff3f69e707
lsiminski <lsiminski@mica.edu>
2017-10-02 16:40:49 -0400

    Again

--- list.js
+++ list.js
@@ -1,13 +1,11 @@
 $.getJSON('list.json', function(data) { 
 
   var d = new Date();
-  var mo = d.getMonth();
-  var da = d.getDate ();
-  var moda = mo + 1 +"-" + da;
-  var t = d.getTime ();
+  var n = d.getUTCHours();
+  var moda = n - 4;
     
   $.each(data, function(data) {
-	  if ( this.date === moda ) {
+	  if ( this.hour === moda ) {
 		  $("#list_name p").html("<a href='" + this.url + "' target=blank>" + this.name + "</a>");
 		  $("#list_day p").text(this.day);
 		  $("#list_category p").text(this.category);
