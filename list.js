d8b4cd8e346e6f555d3f5ad843aeffca619624a9
lsiminski <lsiminski@mica.edu>
2017-10-02 16:14:45 -0400

    Removed BG image

--- list.js
+++ list.js
@@ -11,7 +11,7 @@ $.getJSON('list.json', function(data) {
 		  $("#list_name p").html("<a href='" + this.url + "' target=blank>" + this.name + "</a>");
 		  $("#list_day p").text(this.day);
 		  $("#list_category p").text(this.category);
-		  $("body.random").css("background-image","url(" + this.image + ")");
+// 		  $("body.random").css("background-image","url(" + this.image + ")");
 	  } else {
 	  }
   });
