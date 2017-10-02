46d34d9825f4696cea5880f20d8a4af4643a8cf8
lsiminski <lsiminski@mica.edu>
2017-10-02 16:10:36 -0400

    Adulting files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# datestuff
+# adulting 
---
+++ list.js
@@ -0,0 +1,21 @@
+$.getJSON('list.json', function(data) { 
+
+  var d = new Date();
+  var mo = d.getMonth();
+  var da = d.getDate ();
+  var moda = mo + 1 +"-" + da;
+  var t = d.getTime ();
+    
+  $.each(data, function(data) {
+	  if ( this.date === moda ) {
+		  $("#list_name p").html("<a href='" + this.url + "' target=blank>" + this.name + "</a>");
+		  $("#list_day p").text(this.day);
+		  $("#list_category p").text(this.category);
+		  $("body.random").css("background-image","url(" + this.image + ")");
+	  } else {
+	  }
+  });
+    
+});
+
+
