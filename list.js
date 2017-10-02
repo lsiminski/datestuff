5b5aa6287cc27265094c05dfdd0ad0b0cabb0a1d
lsiminski <lsiminski@mica.edu>
2017-10-02 17:15:45 -0400

    asdcsac

--- list.js
+++ list.js
@@ -1,16 +1,11 @@
 $.getJSON('list.json', function(data) { 
 
-//  var d = new Date();
-//  var n = d.getHour();
-//  var moda = n + 1;
-
-	Date date = new Date();   // given date
-	Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
-	calendar.setTime(date);   // assigns calendar to given date 
-	calendar.get(Calendar.HOUR_OF_DAY); // gets hour in 24h format
+ var d = new Date();
+ var n = d.getHour();
+ var moda = n;
     
   $.each(data, function(data) {
-	  if ( this.hour === Calendar.HOUR_OF_DAY ) {
+	  if ( this.hour === n ) {
 		  $("#list_name p").html("<a href='" + this.url + "' target=blank>" + this.name + "</a>");
 		  $("#list_day p").text(this.day);
 		  $("#list_hour p").text(this.hour);
