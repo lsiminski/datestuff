52390b1e244812b1529bf7108ab161098cf5055b
lsiminski <lsiminski@mica.edu>
2017-10-02 17:21:36 -0400

    adcasdc

--- list.js
+++ list.js
@@ -4,10 +4,10 @@ $.getJSON('list.json', function(data) {
   var mo = d.getMonth();
   var da = d.getDate ();
   var ho = d.getHours ();
-  var moda = ho + 1;
+//   var moda = ho + 1;
     
   $.each(data, function(data) {
-	  if ( this.hour === moda ) {
+	  if ( this.hour === ho ) {
 		  $("#list_name p").html("<a href='" + this.url + "' target=blank>" + this.name + "</a>");
 		  $("#list_day p").text(this.day);
 		  $("#list_category p").text(this.category);
